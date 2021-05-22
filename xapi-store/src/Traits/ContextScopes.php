249c2c4dd2c3b5c8fbe74325e1a486232f5bd19f
sfraysse <sebastien@fraysse.eu>
2021-05-22 14:25:12 +0200

    moved entity scope

--- xapi-store/src/Traits/ContextScopes.php
+++ xapi-store/src/Traits/ContextScopes.php
@@ -77,39 +77,4 @@ trait ContextScopes
 
         return [['client_id' => $consumer->client_id]];
     }
-    
-    /**
-     * Check if a resource belongs to the consumer scope.
-     *
-     * @param \Trax\Auth\Contracts\HasPermissionsContract  $consumer
-     * @param \Illuminate\Database\Eloquent\Model  $resource
-     * @return bool
-     */
-    protected function entity(HasPermissionsContract $consumer, Model $resource): bool
-    {
-        // When a consumer is not associated with an entity, it works with the owner data.
-        if (is_null($consumer->entity_id)) {
-            return $this->owner($consumer, $resource);
-        }
-
-        // When a consumer is associated with an entity, it works only with this entity data.
-        return $resource->entity_id == $consumer->entity_id;
-    }
-
-    /**
-     * Get a filter for requests to fit with the consumer scope.
-     *
-     * @param \Trax\Auth\Contracts\HasPermissionsContract  $consumer
-     * @return array|null
-     */
-    public function entityFilter(HasPermissionsContract $consumer)
-    {
-        // When a consumer is not associated with an entity, it works with the owner data.
-        if (is_null($consumer->entity_id)) {
-            return $this->ownerFilter($consumer);
-        }
-
-        // When a consumer is associated with an entity, it works only with this entity data.
-        return [['entity_id' => $consumer->entity_id]];
-    }
 }
