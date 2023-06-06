a37ea626ed9a5cec8241205aa916d05feb202f30
Imran Zahoor <mrimranzahoor@gmail.com>
2023-06-06 22:57:46 +0500

    Fixed Undefined offset: 1 exception in date.

--- xapi-store/src/XapiDate.php
+++ xapi-store/src/XapiDate.php
@@ -14,7 +14,7 @@ class XapiDate
         try {
             list($seconds, $micro) = explode('.', microtime(true));
             // We need 4 digits precision.
-            $micro = substr($micro.'000', 0, 4);
+            $micro = substr($micro . '000', 0, 4);
         } catch (\Exception $e) {
             // $micro may be missing.
             $seconds = microtime(true);
@@ -36,6 +36,9 @@ class XapiDate
         // Timezone is taken into account.
         $timestamp = strtotime($isoDate);
 
+        // Append T00:00:00 in case the date string doesn't contain T
+        $isoDate = (stripos($isoDate, 'T') === false) ? $isoDate . 'T00:00:00' : $isoDate;
+
         // We need to extract microseconds from the original ISO date.
         list($date, $time) = explode('T', $isoDate);
 
@@ -47,11 +50,11 @@ class XapiDate
         } elseif (strpos($time, 'Z') !== false) {
             list($time) = explode('Z', $time);
         }
-        
+
         // Extract and normalize microseconds (4 digits precision).
         try {
             list($seconds, $micro) = explode('.', $time);
-            $micro = substr($micro.'000', 0, 4);
+            $micro = substr($micro . '000', 0, 4);
         } catch (\Exception $e) {
             $micro = '0000';
         }
